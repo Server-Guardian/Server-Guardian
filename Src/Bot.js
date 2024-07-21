36379a1bd5b96d8070189b29e47569e603a72cdb
SmidtDeveloping <101642623+SmidtDeveloping@users.noreply.github.com>
2024-07-22 00:50:26 +0200

    Create Bot.js

---
+++ Src/Bot.js
@@ -0,0 +1,22 @@
+1
+// Imports
+const client = require("./botClient")
+const app = require("../dashboard/expressClient")
+require("dotenv").config()
+const port = process.env.PORT || 3001
+
+// Loaders
+const loadCommands = require("./loaders/commands")
+loadCommands()
+const loadEvents = require("./loaders/events")
+loadEvents()
+const loadFunctions = require("./loaders/functions")
+loadFunctions()
+const loadFunctionsDash = require("./loaders/loaddashfunctions")
+loadFunctionsDash()
+// client.on("debug", console.debug)
+client.on("warn", console.warn)
+
+app.listen(port, "0.0.0.0", function () {
+	console.log("✅| Dashboard");
+})
