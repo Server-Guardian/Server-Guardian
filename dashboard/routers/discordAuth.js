703feb7faa95a93c13eb22d7e94b26434599caf3
SmidtDeveloping <101642623+SmidtDeveloping@users.noreply.github.com>
2024-07-22 14:29:46 +0200

    Website Fix

--- dashboard/routers/discordAuth.js
+++ dashboard/routers/discordAuth.js
@@ -8,7 +8,7 @@ app.get("/discord", async  (req, res) => {
     params.append("client_secret", process.env.LOGIN_CLIENT_SECRET);
     params.append("grant_type", "authorization_code");
     params.append("code", code);  
-    params.append("redirect_uri", "localhost:3000/discord");
+    params.append("redirect_uri", "https://serverguardian.developingbyjulian.nl/discord");
 
     try {
         const response = await axios.post('https://discord.com/api/oauth2/token', params);
